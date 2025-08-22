e4eb7947d7012575531ed5148f2b7825cfe774ac
Yuri <prettoyuri7@gmail.com>
2025-08-22 14:26:16 -0700

    hw1 updated

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,7 +1,7 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
         BodyOfWater b = new BodyOfWater("Lake Michigan", 10, 10, false);
-        BodyOfWater b1 = new BodyOfWater ("Pacific Ocean", 20, 5, true);
+        BodyOfWater b1 = new BodyOfWater ("Pacific Ocean",  5);
         b.printState();
         b1.printState();
         b1.setName("Atlantic Ocean");
