db331beb5ae3ed44eaa207b740fc0338a9549aff
Yuri <prettoyuri7@gmail.com>
2025-08-22 13:45:41 -0700

    sample1

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,33 @@
+public class BodyOfWater {
+    private String name;
+    private double largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater (String n, double l, double a, boolean s){
+        name = n;
+        largestDiameter = l;
+        avgDepth = a;
+        isSaltWater = s;
+    }
+
+    public BodyOfWater (String n, double a){
+        name = n;
+        avgDepth = a;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setLargestDiameter(double l){
+        largestDiameter = l;
+    }
+
+    public void printState(){
+        System.out.println("Name: "+ name);
+        System.out.println("Largest Diameter: " + largestDiameter);
+        System.out.println("Average Depth: " + avgDepth);
+        System.out.println("Salt Water?" + isSaltWater);
+    }
+}
